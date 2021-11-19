d1688173bcfd9678d9bf0659995c88df72a0d052
liamSharkey <wsharke@uwo.ca>
2021-11-18 21:55:27 -0500

    cleaned up files and folders. Routing works better now

--- db/updateDB.js
+++ db/updateDB.js
@@ -7,4 +7,15 @@ const connection = mysql.createConnection({
 	database: "usersDB"
 });
 
-module.exports = connection;
+connection.connect();
+
+connection.query(
+	`
+	alter table Person
+	modify userName varchar(50)`,
+	(error, rows, fields) => {
+		console.log(error);
+	}
+);
+
+connection.end();
--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
-const cookieParser = require("cookie-parser");
-
-app.use(cookieParser("secret time"));
+const router = express.Router();
 
 //handlebars middleware
 var hbs = exphbs.create({
@@ -54,8 +52,132 @@ app.get(
 app.use(express.static(path.join(__dirname, "public")));
 
 // Members api routes
-app.use("/api/members", require("./routes/api/members"));
+// app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 1500;
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
+
+//############################################################################################################
+const connection = require("./db/usersDB");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secret time"));
+
+//Get all members
+app.get("/getMembers", (req, res) => {
+	connection.query("SELECT * FROM Person", (error, rows, fields) => {
+		let memberList = [];
+		for (r of rows) {
+			memberList.push({
+				username: r.userName,
+				password: r.password,
+				email: r.email,
+				role: r.role
+			});
+		}
+		res.json(memberList);
+	});
+});
+
+//Get single member by email
+app.get("/:email", (req, res) => {
+	connection.query(
+		`SELECT * FROM Person WHERE email LIKE "${req.query.email.replace(
+			"%40",
+			"@"
+		)}"`,
+		(error, rows, fields) => {
+			let memberList = [];
+			for (r of rows) {
+				memberList.push({
+					username: r.userName,
+					password: r.password,
+					email: r.email,
+					role: r.role
+				});
+			}
+			if (req.query.password === r.password) {
+				res.json(memberList);
+			} else {
+				res.status(400).json({msg: "passwords do not match"});
+			}
+		}
+	);
+});
+
+//login
+app.get(
+	"/:login",
+	(req, res, next) => {
+		connection.query(
+			`SELECT * FROM Person WHERE email LIKE "${req.query.email.replace(
+				"%40",
+				"@"
+			)}"`,
+			(error, rows, fields) => {
+				let memberList = [];
+				for (r of rows) {
+					memberList.push({
+						username: r.userName,
+						password: r.password,
+						email: r.email,
+						role: r.role
+					});
+				}
+				if (req.query.password === r.password) {
+					res.json(memberList);
+					next();
+				} else {
+					res.status(400).json({msg: "passwords do not match"});
+				}
+			}
+		);
+	},
+	(req, res) => {
+		res.cookie("email", req.query.email.replace("%40", "@"));
+		res.cookie("password", req.query.password, {signed: true});
+
+		res.redirect("/myMeetings");
+	}
+);
+
+//Create member
+app.post("/signUp", (req, res) => {
+	const newMember = {
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		role: req.body.role
+	};
+
+	if (!newMember.name || !newMember.email || !newMember.password) {
+		return res
+			.status(400)
+			.json({msg: "Please include a name, password and email"});
+	}
+
+	connection.query(
+		`insert into Person values ("${newMember.name}", "${newMember.password}", "${newMember.email}", "${newMember.role}")`,
+		(error, rows, fields) => {
+			console.log(error);
+		}
+	);
+
+	//determines route after creating member
+	res.redirect("/");
+});
+
+//Delete member
+app.delete("/:email", (req, res) => {
+	connection.query(
+		`DELETE FROM Person WHERE email LIKE "${req.query.email.replace(
+			"%40",
+			"@"
+		)}"`,
+		(error, rows, fields) => {
+			console.log(error);
+			res.status(200).json({msg: "member deleted"});
+		}
+	);
+});
